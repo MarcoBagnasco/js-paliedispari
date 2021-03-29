88d0fc5fbb0212217c8699e0b4fddf4ebbf76a22
MarcoBagnasco <bagna.hbk@gmail.com>
2021-03-29 21:48:58 +0200

    second exercise finished

--- js/script.js
+++ js/script.js
@@ -42,14 +42,58 @@
     5.Valutare se la somma è pari o dispari
     6.Stabilire chi ha vinto
 */
+//User Choices
 var choice;
 do{
     choice = prompt('Pari o Dispari?').toLowerCase().trim();
 } while(choice !== 'pari' && choice !== 'dispari');
-console.log(choice); //test
+console.log('Scelta:', choice);
 
 var userNumber;
 do{
     userNumber = parseInt(prompt('Scegli un numero da 1 a 5').trim());
 } while(isNaN(userNumber) || !(userNumber >= 1 && userNumber <= 5));
-console.log(userNumber); //test
+console.log('Il numero dell\'utente è:', userNumber);
+
+//Computer Number
+var computerNumber = randomNumber(5);
+console.log('Il numero del computer è:', computerNumber);
+
+//Sum
+var sum = computerNumber + userNumber;
+console.log('La somma è:', sum);
+
+//Pari o Dispari
+var result = pariDispari(sum);
+
+if(result === choice){
+    console.log('Complimenti hai vinto!');
+} else {
+    console.log('Mi dispiace hai perso');
+}
+
+
+// FUNCTIONS
+
+/**
+ * Crea un numro random da 1 al valore passato
+ * @param {number} range - numero massimo
+ * @returns - numero random
+ */
+function randomNumber(range){
+    var num = Math.floor(Math.random() * range) + 1;
+    return num;
+}
+
+/**
+ * Stabilisce se il nummero passato è pari o dispari
+ * @param {number} num - numero da valutare
+ * @returns - pari o dispari
+ */
+function pariDispari(num){
+    if(num % 2 === 0) {
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
